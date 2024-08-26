8537db2ecf3c77ee698a558b0fed2143ba3d1b12
GK1431 <boddugopal143@gmail.com>
2024-08-26 16:25:09 +0000

    create a script

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Updating system"
+sudo apt update -y
+sleep 5
+
+echo "install Zip unzip"
+sudo apt install -y zip unzip
+sleep 5
+
+echo " install http"
+sudo apt install -y nginx
+sleep 5
+
+echo "remove sample pages"
+sudo rm -rf /var/www/html
+sleep 5
+
+echo "clone the login app"
+sudo git clone https://github.com/Aj-ji/EMD.git /var/www/html
+sleep 5
+
+echo "pramodh has ended"
+
